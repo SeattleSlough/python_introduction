8ff478da15a08397f830f0a43546a3f545bb4133
Michael J Maggs <seattleslew@runawayserver.com>
2023-04-02 18:08:59 -0700

    re-do of initial lesson

---
+++ numbers_course.py
@@ -0,0 +1,20 @@
+# Fundamental data types
+
+# int and float
+# float stores binaries on L and R sides of decimal in different locations in memort
+# float more memory expensive than int
+
+print(type(2 + 2))
+
+print(type(2 - 4))
+print(type(2 / 4))
+print(type(9 + 1.2))
+print(
+    type(9.8 + 1.2)
+)  # still a float even though it total a whole because decimal point kept
+
+print(2**2)  # exponential
+print(5 // 4)  # 1 --> // is hard divide
+print(4 // 5)  # 0 as it is less than 1
+print(5 % 4)
+print(6 % 4)
